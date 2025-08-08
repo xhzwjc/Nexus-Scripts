cba48e5ef3b4126fcef6df44a338be5117b41e1e
xhzwjc <w2983423303@163.com>
2025-08-08 16:07:36 +0800

    优化前后端dockerfile配置文件

--- fastApiProject/run.py
+++ fastApiProject/run.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # 解析命令行参数
     parser = argparse.ArgumentParser(description="启动春苗系统结算API服务")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="服务绑定的主机地址")
-    parser.add_argument("--port", type=int, default=8000, help="服务监听的端口号")
+    parser.add_argument("--port", type=int, default=3000, help="服务监听的端口号")
     parser.add_argument("--reload", action="store_true", help="开发模式下自动重载")
     args = parser.parse_args()
 
